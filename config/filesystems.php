b113407406bd7d8b34a353d72f33915fa326d9aa
Hafiz Hidayat <154106060+tuanthanh2005@users.noreply.github.com>
2026-07-01 20:37:05 +0700

    Add public_uploads disk with dynamic environment support

--- config/filesystems.php
+++ config/filesystems.php
@@ -47,6 +47,14 @@ return [
             'report' => false,
         ],
 
+        'public_uploads' => [
+            'driver' => 'local',
+            'root' => is_dir(base_path('../public_html')) ? base_path('../public_html') : public_path(),
+            'visibility' => 'public',
+            'throw' => false,
+            'report' => false,
+        ],
+
         's3' => [
             'driver' => 's3',
             'key' => env('AWS_ACCESS_KEY_ID'),
